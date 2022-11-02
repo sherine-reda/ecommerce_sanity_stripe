98adb63610f45963a2d80cb463a377e2c7ab04ac
sherine-reda <83099016+sherine-reda@users.noreply.github.com>
2022-11-02 10:33:03 +0200

    first commit

---
+++ pages/api/stripe.js
@@ -0,0 +1,52 @@
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      const params = {
+        submit_type: 'pay',
+        mode: 'payment',
+        payment_method_types: ['card'],
+        billing_address_collection: 'auto',
+        shipping_options: {
+            "0": { shipping_rate: 'shr_1LzK09EgfGkuQkoqtdQl4fbr' },
+        },
+        line_items: req.body.map((item) => {
+          const img = item.image[0].asset._ref;
+          {console.log(img)}
+          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/t441kw9b/production/').replace('-webp', '.webp');
+
+          return {
+            price_data: { 
+              currency: 'usd',
+              product_data: { 
+                name: item.name,
+                images: [newImage],
+              },
+              unit_amount: item.price * 100,
+            },
+            adjustable_quantity: {
+              enabled:true,
+              minimum: 1,
+            },
+            quantity: item.quantity
+          }
+        }),
+        success_url: "http://localhost:3000/success",
+        cancel_url: "http://localhost:3000/canceled",
+      }
+
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create(params);
+
+      res.status(200).json(session);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+  }
+}
